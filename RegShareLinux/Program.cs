bced6698ce2bb4ebedb665ba45061bc04a670325
KinKonG <KinKonG@DESKTOP-E7MLMM1>
2022-11-03 12:00:58 +0300

    Добавьте файлы проекта.

---
+++ RegShareLinux/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegShareLinux
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Registry.LocalMachine.CreateSubKey(@"SYSTEM\CurrentControlSet\Control\Lsa").SetValue("everyoneincludesanonymous", 1);
+            Registry.LocalMachine.CreateSubKey(@"SYSTEM\ControlSet001\Services\LanmanServer\Parameters").SetValue("NullSessionShares", new string[] { "Common" }, RegistryValueKind.MultiString);
+            Registry.LocalMachine.CreateSubKey(@"SYSTEM\ControlSet001\Control\Lsa\").SetValue("everyoneincludesanonymous", 1);
+            Registry.LocalMachine.CreateSubKey(@"SYSTEM\ControlSet001\Services\LanmanServer\Parameters").SetValue("NullSessionShares", new string[] { "Common" }, RegistryValueKind.MultiString);
+            
+        }
+    }   
+}
